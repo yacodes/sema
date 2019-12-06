ef91c1c777d842d4e2098beba7e934e01aa1f21a
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2019-12-06 12:11:41 +0100

    format nnRegression

--- src/machineLearning/tfjs/nnRegression/nnRegression.tf
+++ src/machineLearning/tfjs/nnRegression/nnRegression.tf
@@ -12,10 +12,14 @@ ____
 
 ______
 
-onMouseMove = (x,y) =>{if (recording) {console.log(y);
-                                       trainingInput[trainingInput.length] = x;
-                                       trainingInput[trainingInput.length] = y;
-                                      trainingOutput[trainingOutput.length] = target};}
+onMouseMove = (x,y) => {
+  if (recording) {
+    console.log(y);
+    trainingInput[trainingInput.length] = x;
+    trainingInput[trainingInput.length] = y;
+    trainingOutput[trainingOutput.length] = target
+  };
+}
 ______
 //js
 
@@ -27,12 +31,16 @@ model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 //set up the training data set
 xs = tf.tensor2d(trainingInput, [trainingInput.length/2,2]);
 ys = tf.tensor2d(trainingOutput, [trainingOutput.length,1]);
+
 //var xs = tf.tensor2d([0, 1, 2, 3, 4, 5], [6, 1]);
 //var ys = tf.tensor2d([0, 50, 100, 150, 200, 250], [6, 1]);
 
 //train the model on the data set
 trainingRecord =0;
-model.fit(xs, ys, { epochs: 500 }).then(result => {console.log(`Model trained`); console.log(result); trainingRecord = result;});
+model.fit(xs, ys, { epochs: 500 }).then(result => { 
+                      console.log(`Model trained`); 
+                      console.log(result); 
+                      trainingRecord = result;});
 _____
 
 trainingRecord.history.loss
@@ -49,9 +57,12 @@ ___
 
 predicting=1;
 networkOutput = [0];
-onMouseMove = (x,y) =>{if (predicting) {networkOutput = test([x,y]);
-                                        console.log(networkOutput);
-                                       }}
+onMouseMove = (x,y) => { 
+  if (predicting) {
+    networkOutput = test([x,y]);
+    console.log(networkOutput);
+  }
+}
 
 __________
 //route the model predictions back to the live coding environment
